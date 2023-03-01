c6a1d0146838451c3deecec73e39ab4db38094f3
João Ribas <ribas.joao1410@gmail.com>
2023-02-28 23:24:35 -0300

    other browsers

--- classes/class01/pages/video-player/src/factory.js
+++ classes/class01/pages/video-player/src/factory.js
@@ -6,17 +6,36 @@ import View from "./view.js"
 
 async function getWorker() {
   if(supportsWorkerType()) {
-    console.log('Suporta')
+    console.log('initializing esm workers')
     const worker = new Worker(`./src/worker.js`, {type: "module"})
     return worker;
   } 
+  console.warn(`Your browser doesn't support esm modules on webworkers!`)
+  console.warn('Importing libraries...')
+  await import("https://unpkg.com/@tensorflow/tfjs-core@2.4.0/dist/tf-core.js")
+  await import("https://unpkg.com/@tensorflow/tfjs-converter@2.4.0/dist/tf-converter.js")
+  await import("https://unpkg.com/@tensorflow/tfjs-backend-webgl@2.4.0/dist/tf-backend-webgl.js")
+  await import("https://unpkg.com/@tensorflow-models/face-landmarks-detection@0.0.1/dist/face-landmarks-detection.js")
+
+  console.warn(`using worker mock instead!`)
+  const service = new Service({
+    faceLandmarksDetection: window.faceLandmarksDetection
+  })
 
   const workerMock = {
-    async postMessage() {},
+    async postMessage(video) {
+      const blinked = await service.handBlinked(video)
+      if(!blinked) return;
+      workerMock.onmessage({ data: { blinked }})
+    },
+    // vai ser sobreescrito pela controller
     onmessage(msg) {}
   }
+  console.log('loading tf model...')
+  await service.loadModel()
+  console.log('tf model loaded...')
 
-  console.log('Nao suporta')
+  setTimeout(() => worker.onmessage({ data: 'READY' }), 500);
   return workerMock
 }
 
